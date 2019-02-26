216a995ce4aed8c7d56c28cd3166645ebe0ffbfb
luodexun <lll279906908@outlook.com>
2019-02-26 15:50:16 +0800

    test

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -7,7 +7,6 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 class Goods extends Base {
     public function index($params){
-         $str=base64_encode();
         $decrypted=openssl_decrypt('HdTdR6u9KbW8nXsOnxKmKQ==',"AES-128-CBC",'8NONwyJtHesysWpM',OPENSSL_ZERO_PADDING,'12345678a0123f56');
         dump($decrypted);exit();
         $condition = [
