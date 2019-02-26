d1b12aea155e7167218aad845b5598e28eb6a461
luodexun <lll279906908@outlook.com>
2019-02-26 16:26:29 +0800

    test

--- application/route.php
+++ application/route.php
@@ -10,10 +10,6 @@
 // +----------------------------------------------------------------------
 use think\Route;
 $Action=request()->isOptions()?'Index/index':input("post.Action");
-if(request()->isPost()){
-    $decrypted = openssl_decrypt('GTdDSZ5+ly40Dp2FQz41N1rrH+GbTaljjHxhqVFcn0HyulEa3dFCAs9rfV41hh5q', 'AES-128-CBC', '8NONwyJtHesysWpM', 2,'12345678a0123f56');
-    return $decrypted;
-}
 $url='api/'.$Action;
 Route::domain('api',$url);
 Route::domain('www','admin');
