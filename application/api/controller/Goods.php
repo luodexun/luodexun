5b19c14e2dd07569d1a471752601ee64731f6b1e
luodexun <lll279906908@outlook.com>
2019-02-26 16:17:50 +0800

    test

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -7,6 +7,7 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 class Goods extends Base {
     public function index($params){
+        phpversion();
         $decrypted=openssl_decrypt('GTdDSZ5+ly40Dp2FQz41N1rrH+GbTaljjHxhqVFcn0HyulEa3dFCAs9rfV41hh5q',"AES-128-CBC",'8NONwyJtHesysWpM',2,'12345678a0123f56');
         dump($decrypted);exit();
         $condition = [
