432ee4a1112d960911db49e7dfd9b81aa4d103e9
luodexun <lll279906908@outlook.com>
2019-02-26 16:29:07 +0800

    test

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -9,7 +9,7 @@ class Goods extends Base {
     public function index($params){
 
         dump(phpversion());
-        $decrypted = openssl_decrypt('GTdDSZ5+ly40Dp2FQz41N1rrH+GbTaljjHxhqVFcn0HyulEa3dFCAs9rfV41hh5q', 'AES-128-CBC', '8NONwyJtHesysWpM', 1,'12345678a0123f56');
+        $decrypted = openssl_decrypt('GTdDSZ5+ly40Dp2FQz41N1rrH+GbTaljjHxhqVFcn0HyulEa3dFCAs9rfV41hh5q', 'AES-128-CBC', '8NONwyJtHesysWpM', OPENSSL_ZERO_PADDING,'12345678a0123f56');
         var_dump($decrypted);exit();
         $condition = [
             'store'=> ['gt', $params['store']]
