84d6900ba4c0d589f02914519b4ee9f4be85af9b
luodexun <lll279906908@outlook.com>
2019-02-27 17:14:55 +0800

    test

--- application/route.php
+++ application/route.php
@@ -9,15 +9,14 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 use think\Route;
+use AES\Load;
 $url=null;
 if(request()->isOptions()){
     $url='api/Index/index';
 }else if(request()->isPost()){
-    $header=json_decode(openssl_decrypt(request()->header('sign'), 'AES-128-CBC', '07bad5311d5108d4', 4,'fbd19b9cae6615a1'),true);
-    $decrypted = openssl_decrypt(file_get_contents("php://input"), 'AES-128-CBC', substr($header['sign'],0,16), 4,substr($header['sign'],16,16));
-    $data=json_decode($decrypted,true);
-    $url='api/'.$data['Action'];
-    $_POST['params']=$data['params'];
+    $address=&$url;
+    $aes=new Load();
+    $aes->run($address);
 }
 Route::domain('api',$url);
 Route::domain('www','admin');
--- extend/AES/Load.php
+++ extend/AES/Load.php
@@ -11,10 +11,10 @@ class Load{
     public function __construct(){
 
        }
-    public function run(){
-           $decrypted = openssl_decrypt(file_get_contents("php://input"), 'AES-128-CBC', '8NONwyJtHesysWpM', 4,'12345678a0123f56');
-           $data=json_decode($decrypted,true);
-           $url='api/'.$data['Action'];
-           $_POST['params']=$data['params'];
+    public function run($address){
+        $header=json_decode(openssl_decrypt(request()->header('sign'), 'AES-128-CBC', '07bad5311d5108d4', 4,'fbd19b9cae6615a1'),true);
+        $data=json_decode(openssl_decrypt(file_get_contents("php://input"), 'AES-128-CBC', substr($header['sign'],0,16), 4,substr($header['sign'],16,16)),true);
+        $address='api/'.$data['Action'];
+        $_POST['params']=$data['params'];
        }
 }
