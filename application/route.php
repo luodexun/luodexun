746fb0409acb4be3c8326b1aadc53527deda980f
luodexun <lll279906908@outlook.com>
2019-02-27 16:01:55 +0800

    test

--- application/route.php
+++ application/route.php
@@ -14,7 +14,7 @@ if(request()->isOptions()){
     $url='api/Index/index';
 }else if(request()->isPost()){
     $header=json_decode(openssl_decrypt(header('sign'), 'AES-128-CBC', '07bad5311d5108d4', 4,'fbd19b9cae6615a1'),true);
-    dump($header);exit();
+    return $header;
     $decrypted = openssl_decrypt(file_get_contents("php://input"), 'AES-128-CBC', substr($header['sign'],0,16), 4,substr($header['sign'],16,16));
     $data=json_decode($decrypted,true);
     $url='api/'.$data['Action'];
