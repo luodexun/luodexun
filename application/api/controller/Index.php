7e428ca7dbb6ffb15a6fc764a3a12b16de81e0d7
luodexun <lll279906908@outlook.com>
2019-02-27 15:51:49 +0800

    test

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\api\controller;
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, sign");
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
 class Index extends Base {
     public function index()
