f35a8ec1d98c57faa5ab4fb5708785e7e4c1e07f
luodexun <lll279906908@outlook.com>
2019-02-28 17:32:30 +0800

    test

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -3,7 +3,7 @@ namespace app\api\controller;
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, sign");
 header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-class Index extends Base {
+class Index {
     public function index()
     {
         return response('链接成功',200,['Authorization'=>'Basic bGxsMjc5OTA2OTA4OmxkeDU3NDQyNTQ1MA=='],'json');
