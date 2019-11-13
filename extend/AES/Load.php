bc7ff41c741a0cd5f99e5a0b38093accd5e99d06
luodexun <lll279906908@outlook.com>
2019-11-13 16:35:00 +0800

    Aes

--- extend/AES/Load.php
+++ extend/AES/Load.php
@@ -11,6 +11,7 @@ class Load{
     public function run($address){
         $header=json_decode(openssl_decrypt(request()->header('sign'), 'AES-128-CBC', '07bad5311d5108d4', 4,'fbd19b9cae6615a1'),true);
         $data=json_decode(openssl_decrypt(file_get_contents("php://input"), 'AES-128-CBC', substr($header['sign'],0,16), 4,substr($header['sign'],16,16)),true);
+        dump($data);exit();
         $address['url']='api/'.$data['Action'];
         session('app_id',$header['app_id']);
         session('time',$header['time']);
